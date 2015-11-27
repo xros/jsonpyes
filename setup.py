4fb91440b768125cb769b895b33db590f5d6ea55
xxros <liusongh@gmail.com>
2015-11-28 00:27:40 +0800

    make it work in console

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup 
 
 setup(name='jsonpyes',
-        version='1.1.4',
+        version='1.1.5',
         author="Alexander Liu",
         license="GPL V3",
         description="Import JSON raw data to ElasticSearch using Python in one line of commands",
         platforms=["Unix","Linux","OSX","Android","Windows"],
         url="https://github.com/xros/json-py-es",
-        download_url="https://github.com/xros/json-py-es/tarball/1.1.4",
+        download_url="https://github.com/xros/json-py-es/tarball/1.1.5",
         #py_modules=['elasticsearch','jsonpyes','simplejson'],
         install_requires=[
             'elasticsearch',
