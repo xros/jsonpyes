d9ee7306722492724658fb9cf60bc15523adac29
xxros <liusongh@gmail.com>
2016-03-31 01:13:23 +0800

    add FTE license

--- jsonpyes.py
+++ jsonpyes.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python2
 # encoding: utf-8
+"""
+                          __________________
+                         / ____/_  __/ ____/
+                        / /_    / / / __/
+                       / __/   / / / /___
+                      /_/     /_/ /_____/
+
+                   FREER THAN EVER PUBLIC LICENSE
+
+                        Version 1, March 2016
+
+             Copyright (C) 2016 Alexander Liu <alex(at)nervey.com>
+
+If there exists a most free license, this will be freer than it.
+Everyone is permitted to copy, distribute or modifiy anything under this license.
+
+                   FREER THAN EVER PUBLIC LICENSE
+    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+0. You are freer than ever to copy, distribute and modifiy anything under this license.
+"""
+
 
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
